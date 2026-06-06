a5cf45eb93fe32b36f4c5a1ac10118df8c1e2462
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-06-06 20:22:26 +0530

    Error in controller layer

--- backend/src/main/java/com/opsmind/core/OpsMindApplication.java
+++ backend/src/main/java/com/opsmind/core/OpsMindApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 /**
  * OpsMind Backend Application - Main Entry Point
  * 
