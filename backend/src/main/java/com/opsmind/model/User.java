8440d5ef79530a6fa429b1c13da70856e38cdfdd
Yashwardhan Kumar <yashwardhankumar15@gmail.com>
2026-06-07 02:03:57 +0530

    Database changed

---
+++ backend/src/main/java/com/opsmind/model/User.java
@@ -0,0 +1,35 @@
+package com.opsmind.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String organizationName;
+}
